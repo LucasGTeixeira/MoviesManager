829e2c189904fa9fb35134a0165920bac794d4eb
Lucas Gabriel Teixeira da Silva <89523528+LucasGTeixeira@users.noreply.github.com>
2022-12-04 14:50:50 -0300

    show initialized movieList on activityMain

--- app/src/main/java/com/lucasgteixeira/moviesmanager/model/Constant.kt
+++ app/src/main/java/com/lucasgteixeira/moviesmanager/model/Constant.kt
@@ -1,4 +1,5 @@
 package com.lucasgteixeira.moviesmanager.model
 
 object Constant {
+    const val EXTRA_MOVIE = "EXTRA_MOVIE"
 }
--- app/src/main/java/com/lucasgteixeira/moviesmanager/view/MainActivity.kt
+++ app/src/main/java/com/lucasgteixeira/moviesmanager/view/MainActivity.kt
@@ -1,16 +1,54 @@
 package com.lucasgteixeira.moviesmanager.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.lucasgteixeira.moviesmanager.adapter.MovieAdapter
 import com.lucasgteixeira.moviesmanager.databinding.ActivityMainBinding
+import com.lucasgteixeira.moviesmanager.model.Constant.EXTRA_MOVIE
+import com.lucasgteixeira.moviesmanager.model.Movie
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val movieList: MutableList<Movie> = mutableListOf()
+
+    private lateinit var movieAdapter: MovieAdapter
+
+    private lateinit var marl: ActivityResultLauncher<Intent>
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        fillMovieList()
+
+        movieAdapter = MovieAdapter(this, movieList)
+        amb.movieLv.adapter = movieAdapter
+
+        movieAdapter.notifyDataSetChanged()
+    }
+
+    private fun fillMovieList() {
+        for (i in 1..5) {
+            movieList.add(
+                Movie(
+                    id = i,
+                    name = "filme $i",
+                    releaseYear = "20$i",
+                    studio = "studio",
+                    duration = "60",
+                    flag = "assitido",
+                    rating = "5",
+                    genra = "terror",
+                )
+            )
+        }
     }
 }
